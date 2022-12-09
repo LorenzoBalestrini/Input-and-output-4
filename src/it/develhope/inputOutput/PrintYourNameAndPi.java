a8dd28f2b8855fafde5770d310a6d88035778d88
Lorenzo Balestrini <lo.bale@libero.it>
2022-12-09 16:09:17 +0100

    input and output 4

---
+++ src/it/develhope/inputOutput/PrintYourNameAndPi.java
@@ -0,0 +1,15 @@
+package it.develhope.inputOutput;
+
+import java.util.Scanner;
+
+public class PrintYourNameAndPi {
+
+    public static void main(String[] args) {
+
+
+        System.out.printf("%s%n", "LORENZO BALESTRINI");
+        System.out.printf("The number pi is: %f", 3.141590);
+
+    }
+}
+
